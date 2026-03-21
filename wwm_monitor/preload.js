16551144c34baaa8e63efad0a849a3d4ad07e4aa
Onyx <onyxdigitalshop@gmail.com>
2026-03-21 04:42:23 +0100

    Delete wwm_monitor directory

--- wwm_monitor/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize:    () => ipcRenderer.send('minimize'),
-  close:       () => ipcRenderer.send('close'),
-  send:        (ch, val) => ipcRenderer.send(ch, val),
-  getStartup:  () => ipcRenderer.invoke('get-startup'),
-})
